f9f66d2d89db3b2155db595a765631d892527384
BahaniBillel <billel.bahani@gmail.com>
2022-10-22 20:58:03 +0100

    product components feed

--- pages/components/Product.jsx
+++ pages/components/Product.jsx
@@ -1,30 +1,49 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import { StarIcon } from "@heroicons/react/24/solid";
-
-const MAX_RATING = 5;
-const MIN_RATING = 1;
+import ReactStars from "react-rating-stars-component";
+import CurrencyFormat from "react-currency-format";
 
 function Product({ id, title, price, description, category, image }) {
-  const [rating] = useState(
-    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
-  );
-
   const [hasprime] = useState(Math.random() < 0.5);
 
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
   return (
-    <div className="">
-      <p>{category}</p>
-      <Image src={image} height={200} width={200} objectFit="contain" />
-      <h4>{title}</h4>
-      <div className="flex">
-        {Array(rating)
-          .fill()
-          .map((_, i) => (
-            <StarIcon className="h-5 text-yellow-500" />
-          ))}
+    <div className=" relative flex flex-col  bg-white z-30 p-10 m-5 space-y-2 ">
+      <div className="flex flex-row  text-xs   ">
+        <div className="flex-grow"></div>
+        <p className=" flex-none text-gray-500 italic">{category}</p>
       </div>
-      {hasprime}
+
+      <div className="w-full ">
+        <Image src={image} height={300} width={300} objectFit="contain" />
+      </div>
+
+      <div className="relative  flex-col space-y-5  ">
+        <h4 className="text-md font-bold flex">{title}</h4>
+        <p className="text-sm text-gray-500 hover:text-black">{description}</p>
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={24}
+          isHalf={true}
+          emptyIcon={<i className="far fa-star"></i>}
+          halfIcon={<i className="fa fa-star-half-alt"></i>}
+          fullIcon={<i className="fa fa-star"></i>}
+          activeColor="#ffd700"
+          className=""
+        />
+        <CurrencyFormat
+          value={price}
+          suffix={" DZD"}
+          className="flex-none font-bold"
+        />
+      </div>
+      <div className="flex-grow"></div>
+      <button className="mt-auto button"> Add to Basket</button>
     </div>
   );
 }
--- pages/components/ProductFeed.jsx
+++ pages/components/ProductFeed.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 
 function ProductFeed({ products }) {
   return (
-    <div className="text-xs">
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 text-sx">
       {products.map(({ id, title, price, description, category, image }) => (
         <Product
           key={id}
